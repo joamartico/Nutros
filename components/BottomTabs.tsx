55c4d9e522da1b6b911d794343d7701cda9b2982
joamartico <joamartico@gmail.com>
2022-07-30 12:22:13 -0300

    remove tabbar transluscent and name props

--- components/BottomTabs.tsx
+++ components/BottomTabs.tsx
@@ -3,18 +3,18 @@ import React from 'react'
 const BottomTabs = () => {
 	return (
 		<ion-tabs>
-			<ion-tab-bar slot="bottom" transluscent={true}>
-				<ion-tab-button tab="explore" href="/explore" name="explore">
+			<ion-tab-bar slot="bottom" >
+				<ion-tab-button tab="explore" href="/explore" >
 					<ion-icon name="parthenon" />
 					<ion-label>Explore</ion-label>
 				</ion-tab-button>
 
-				<ion-tab-button tab="me" name="me" href="/me">
+				<ion-tab-button tab="me"  href="/me">
 					<ion-icon name={"person"} />
 					<ion-label>Me</ion-label>
 				</ion-tab-button>
 
-				<ion-tab-button tab="debates" name="debates" href="/debates">
+				<ion-tab-button tab="debates" href="/debates">
 					<ion-icon name={"chatbubbles"} />
 					<ion-label>Debates</ion-label>
 				</ion-tab-button>
