d3e76f2f8fb9f639c2cec852122ddd1a2892c1fb
Joa Marticorena <joamartico@gmail.com>
2023-03-20 00:50:17 -0300

    add camera to camera tab

--- tab-screens/CameraScreen.js
+++ tab-screens/CameraScreen.js
@@ -1,62 +1,86 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
 
 const CameraScreen = ({ selectedTab, foodData }) => {
-	useEffect(() => {
-	  foodData.forEach(food => {
-		console.log(food.description.split(",")[0] + ': ' + food.emoji )
-	  })
-	}, [])
+
+	const videoRef = useRef();
+
+	const getVideo = () => {
+		navigator.mediaDevices
+			.getUserMedia({
+				video: { facingMode: "environment" },
+				audio: false,
+			})
+			.then((stream) => {
+				let video = videoRef.current;
+				console.log(stream);
+				video.srcObject = stream;
+				video.play();
+			})
+			.catch((err) => console.log(err));
+	};
+
+	// useEffect(() => {
+	//   foodData.forEach(food => {
+	// 	console.log(food.description.split(",")[0] + ': ' + food.emoji )
+	//   })
+	// }, [])
 	
-	function generate() {
-		const newFoods = [];
+	// function generate() {
+	// 	const newFoods = [];
 
-		foodData.forEach((food, i) => {
-			setTimeout(() => {
-				const foodName = food.description.split(",")[0];
+	// 	foodData.forEach((food, i) => {
+	// 		setTimeout(() => {
+	// 			const foodName = food.description.split(",")[0];
 
-				const requestOptions = {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer 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`,
-					},
-					body: JSON.stringify({
-						model: "text-davinci-003",
-						prompt: `Convert food into food emoji.\n\nSpinach:🥬 \nOrange:🍊 \n${foodName}:`,
-						temperature: 0.8,
-						max_tokens: 5,
-						top_p: 1.0,
-						frequency_penalty: 0.0,
-						presence_penalty: 0.0,
-						stop: ["\n"],
-					}),
-					// body: JSON.stringify({
-					// 	model: "text-davinci-003",
-					// 	prompt: `Convert food name into a emoji.\n\nSpinach: 🥬 \nBeans: 🫘 \nSausage: 🌭 \n${foodName}:`,
-					// 	temperature: 0.8,
-					// 	max_tokens: 30,
-					// 	top_p: 1.0,
-					// 	frequency_penalty: 0.0,
-					// 	presence_penalty: 0.0,
-					// 	stop: ["\n"],
-					// }),
-				};
+	// 			const requestOptions = {
+	// 				method: "POST",
+	// 				headers: {
+	// 					"Content-Type": "application/json",
+	// 					Authorization: `Bearer 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`,
+	// 				},
+	// 				body: JSON.stringify({
+	// 					model: "text-davinci-003",
+	// 					prompt: `Convert food into food emoji.\n\nSpinach:🥬 \nOrange:🍊 \n${foodName}:`,
+	// 					temperature: 0.8,
+	// 					max_tokens: 5,
+	// 					top_p: 1.0,
+	// 					frequency_penalty: 0.0,
+	// 					presence_penalty: 0.0,
+	// 					stop: ["\n"],
+	// 				}),
+	// 				// body: JSON.stringify({
+	// 				// 	model: "text-davinci-003",
+	// 				// 	prompt: `Convert food name into a emoji.\n\nSpinach: 🥬 \nBeans: 🫘 \nSausage: 🌭 \n${foodName}:`,
+	// 				// 	temperature: 0.8,
+	// 				// 	max_tokens: 30,
+	// 				// 	top_p: 1.0,
+	// 				// 	frequency_penalty: 0.0,
+	// 				// 	presence_penalty: 0.0,
+	// 				// 	stop: ["\n"],
+	// 				// }),
+	// 			};
 
-				fetch("https://api.openai.com/v1/completions", requestOptions)
-					.then((response) => response.json())
-					.then((data) => {
-						console.log(data);
-						console.log(foodName + ": " + data.choices[0].text);
-						foodData[i].emoji = data.choices[0].text;
-					})
-					.catch((error) => console.log(error));
-			}, 700 * i);
-		});
-	}
+	// 			fetch("https://api.openai.com/v1/completions", requestOptions)
+	// 				.then((response) => response.json())
+	// 				.then((data) => {
+	// 					console.log(data);
+	// 					console.log(foodName + ": " + data.choices[0].text);
+	// 					foodData[i].emoji = data.choices[0].text;
+	// 				})
+	// 				.catch((error) => console.log(error));
+	// 		}, 700 * i);
+	// 	});
+	// }
+
+useEffect(() => {
+  selectedTab == 'camera' && getVideo()
+}, [selectedTab])
+
 
 	return (
 		<>
-			<ion-header translucent>
+			<ion-header>
 				<ion-toolbar>
 					<ion-title>Photo your food</ion-title>
 				</ion-toolbar>
@@ -65,32 +89,30 @@ const CameraScreen = ({ selectedTab, foodData }) => {
 			<ion-content fullscreen class="ion-padding">
 				<ion-header collapse="condense">
 					<ion-toolbar>
-						<ion-title size="large">Photo your food</ion-title>
+						<ion-title onClick={() => getVideo()} size="large">Photo your food</ion-title>
 					</ion-toolbar>
 				</ion-header>
 
-				<p
-					onClick={() => {
-						generate();
-					}}
-				>
-					Convert
-				</p>
 
-				<br />
-				<br />
-				<br />
-
-				<p
-					onClick={() => {
-						console.log(foodData);
-					}}
-				>
-					In development 😉
-				</p>
+				<CameraVideo
+					ref={videoRef}
+					muted
+					autoPlay={true}
+					playsInline={true}
+				/>
 			</ion-content>
 		</>
 	);
 };
 
 export default CameraScreen;
+
+
+const CameraVideo = styled.video`
+	position: fixed;
+	top: 0;
+	left: 50%;
+	transform: translate(-50%);
+	height: 100vh;
+	width: auto;
+`;
