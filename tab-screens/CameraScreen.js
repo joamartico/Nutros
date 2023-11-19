977f16ca1034f89997a0a67619a1be9797efd534
Joa Marticorena <joamartico@gmail.com>
2023-11-19 16:00:31 -0300

    change camera code

--- tab-screens/CameraScreen.js
+++ tab-screens/CameraScreen.js
@@ -2,27 +2,79 @@ import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
 const CameraScreen = ({ selectedTab, foodData }) => {
-	const videoRef = useRef(null);
-	const canvasRef = useRef(null);
+	const videoRef = useRef();
+
+	const getVideo = () => {
+		navigator.mediaDevices
+			?.getUserMedia({
+				video: { facingMode: "environment" },
+				audio: false,
+			})
+			.then((stream) => {
+				let video = videoRef.current;
+				console.log(stream);
+				video.srcObject = stream;
+				video.play();
+			})
+			.catch((err) => console.log(err));
+	};
+
+	// useEffect(() => {
+	//   foodData.forEach(food => {
+	// 	console.log(food.description.split(",")[0] + ': ' + food.emoji )
+	//   })
+	// }, [])
+
+	// function generate() {
+	// 	const newFoods = [];
+
+	// 	foodData.forEach((food, i) => {
+	// 		setTimeout(() => {
+	// 			const foodName = food.description.split(",")[0];
+
+	// 			const requestOptions = {
+	// 				method: "POST",
+	// 				headers: {
+	// 					"Content-Type": "application/json",
+	// 					Authorization: `Bearer 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`,
+	// 				},
+	// 				body: JSON.stringify({
+	// 					model: "text-davinci-003",
+	// 					prompt: `Convert food into food emoji.\n\nSpinach:🥬 \nOrange:🍊 \n${foodName}:`,
+	// 					temperature: 0.8,
+	// 					max_tokens: 5,
+	// 					top_p: 1.0,
+	// 					frequency_penalty: 0.0,
+	// 					presence_penalty: 0.0,
+	// 					stop: ["\n"],
+	// 				}),
+	// 				// body: JSON.stringify({
+	// 				// 	model: "text-davinci-003",
+	// 				// 	prompt: `Convert food name into a emoji.\n\nSpinach: 🥬 \nBeans: 🫘 \nSausage: 🌭 \n${foodName}:`,
+	// 				// 	temperature: 0.8,
+	// 				// 	max_tokens: 30,
+	// 				// 	top_p: 1.0,
+	// 				// 	frequency_penalty: 0.0,
+	// 				// 	presence_penalty: 0.0,
+	// 				// 	stop: ["\n"],
+	// 				// }),
+	// 			};
+
+	// 			fetch("https://api.openai.com/v1/completions", requestOptions)
+	// 				.then((response) => response.json())
+	// 				.then((data) => {
+	// 					console.log(data);
+	// 					console.log(foodName + ": " + data.choices[0].text);
+	// 					foodData[i].emoji = data.choices[0].text;
+	// 				})
+	// 				.catch((error) => console.log(error));
+	// 		}, 700 * i);
+	// 	});
+	// }
 
 	useEffect(() => {
-		if (selectedTab === "camera") {
-			// Solicitar acceso a la cámara
-			if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-				navigator.mediaDevices
-					.getUserMedia({ video: true })
-					.then((stream) => {
-						videoRef.current.srcObject = stream;
-						videoRef.current.play();
-					});
-			}
-		}
-	}, []);
-
-	const captureImage = () => {
-		const context = canvasRef.current.getContext("2d");
-		context.drawImage(videoRef.current, 0, 0, 640, 480);
-	};
+		selectedTab == "camera" && getVideo();
+	}, [selectedTab]);
 
 	return (
 		<>
@@ -41,13 +93,11 @@ const CameraScreen = ({ selectedTab, foodData }) => {
 					</ion-toolbar>
 				</ion-header>
 
-				<video ref={videoRef} width="640" height="480" />
-				<button onClick={captureImage}>Capturar Imagen</button>
-				<canvas
-					ref={canvasRef}
-					width="640"
-					height="480"
-					style={{ display: "none" }}
+				<CameraVideo
+					ref={videoRef}
+					muted
+					autoPlay={true}
+					playsInline={true}
 				/>
 			</ion-content>
 		</>
